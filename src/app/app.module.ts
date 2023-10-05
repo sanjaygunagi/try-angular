d93c5bcfed2167b526ec03858e2ee64e901a02ee
sanjay-gunagi <sanjay.gunagi@jobvite-inc.com>
2023-10-05 22:59:33 +0530

    Try angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    RouterLink,
+    RouterOutlet
   ],
   providers: [],
   bootstrap: [AppComponent]
